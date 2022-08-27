97b6178e1831ace02db8443c037545ccd85f9384
bashir-hasanov <beshir.hasanov@gmail.com>
2022-08-26 22:33:04 -0400

    Wiring

--- Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+//@ComponentScan
+ @ComponentScan(basePackages = "com.cydeo")
 public class ConfigCourse {
 
 }
---
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/ConfigCar.java
@@ -0,0 +1,34 @@
+package com.cydeo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigCar {
+
+    @Bean
+    Car car () {
+        Car c = new Car();
+        c.setMake("Honda");
+        return c;
+    }
+
+    // Direct WIRING
+//    @Bean
+//    Person person() {
+//        Person p = new Person();
+//        p.setName("Mike");
+//        p.setCar(car()); // this is (DIRECT) WIRING
+//        return p;
+//    }
+
+    // AUTOWIRING
+    @Bean
+    Person person(Car car) {
+        Person p = new Person();
+        p.setName("Mike");
+        p.setCar(car); // this is AUTOWIRING
+        return p;
+    }
+
+}
