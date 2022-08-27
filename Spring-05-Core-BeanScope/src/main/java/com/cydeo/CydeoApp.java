833572da15dfb907a9560a9c982043754dfc7975
bashir-hasanov <beshir.hasanov@gmail.com>
2022-08-27 09:22:16 -0400

    BeanScope

---
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import com.cydeo.config.ProjectConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+    }
+}
