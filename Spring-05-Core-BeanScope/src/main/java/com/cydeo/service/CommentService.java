458de72b0e4de2a57020caed39f5e3d81fddc3ce
bashir-hasanov <beshir.hasanov@gmail.com>
2022-08-27 09:35:46 -0400

    Eager and @Lazy

--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -3,10 +3,17 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope ("prototype") -> does same thing with the one below
+//@Scope (BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
 public class CommentService {
 
     private final CommentRepository commentRepository;
@@ -16,6 +23,7 @@ public class CommentService {
     public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Hello");
     }
 
     public void publishComment (Comment comment) {
