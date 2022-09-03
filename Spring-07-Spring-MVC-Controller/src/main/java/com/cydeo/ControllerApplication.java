7e5a5f59032e166d2a1f4a686f0ae70ff2875f35
bashir-hasanov <beshir.hasanov@gmail.com>
2022-09-03 12:37:25 -0400

    Spring MVC - Controller

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/ControllerApplication.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(ControllerApplication.class, args);
+    }
+
+}
