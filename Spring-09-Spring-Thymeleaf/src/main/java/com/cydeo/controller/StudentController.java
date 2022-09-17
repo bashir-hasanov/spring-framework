c882b417858e5a8ab8f1a1d92e49a95a43d64481
bashir-hasanov <beshir.hasanov@gmail.com>
2022-09-17 15:31:32 -0400

    Spring Thymeleaf - form

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,14 +3,17 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/student")
+@RequestMapping("/student")  //here in class level we can not replace with @GetMapping
 public class StudentController {
 
-    @RequestMapping("/register")
+    //@RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")  //it is used only in method level, not class level
 public String register(Model model) {
 
         model.addAttribute("students", DataGenerator.createStudent());
