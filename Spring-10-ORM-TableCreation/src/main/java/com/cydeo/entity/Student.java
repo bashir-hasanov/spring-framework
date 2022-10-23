2288eddd57c83acb6aba574373db486a7cbffbce
bashir-hasanov <beshir.hasanov@gmail.com>
2022-10-22 23:16:32 -0400

    Spring ORM - part 2, new annotations & parent-child

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,15 +1,38 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.cydeo.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "studentFirstName")
     private String firstName;  //first_name
+
+    @Column(name = "studentLastName")
     private String lastName;   //last_name
     private String email;
 
+    @Transient
+    private String city;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthdate;
+
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 }
