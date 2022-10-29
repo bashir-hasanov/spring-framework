8fd65022ec4f9629ed9de5750c951a4dfa5cf05d
bashir-hasanov <beshir.hasanov@gmail.com>
2022-10-29 12:56:47 -0400

    Review class - solved Derived Query in CinemaRepository

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -4,18 +4,24 @@ import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to get cinema with a specific name
+    Cinema findByName(String name);
 
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
+    List<Cinema> findTop3BySponsoredNameContainingOrderBySponsoredNameDesc(String sponsoredName);
 
     //Write a derived query to list all cinemas in a specific country
+    List<Cinema> findAllByLocationCountry(String country);
 
     //Write a derived query to list all cinemas with a specific name or sponsored name
+    List<Cinema> findAllByNameOrSponsoredName(String name, String sponsoredName);
 
     // ------------------- JPQL QUERIES ------------------- //
 
