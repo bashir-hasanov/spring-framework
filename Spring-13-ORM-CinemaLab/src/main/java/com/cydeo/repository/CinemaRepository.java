21d31f68e97b0e21d843c14909617357259c78d8
bashir-hasanov <beshir.hasanov@gmail.com>
2022-11-05 11:34:45 -0400

    Review class - completed queries in Genre, Cinema, MovieCinema

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -2,7 +2,10 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
@@ -26,15 +29,25 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name from Cinema c where c.id = ?1")
+    String readCinemaById(Long id);
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all cinemas by location country
-
+    @Query(value = "SELECT * FROM cinema c JOIN location l on c.location_id = l.id WHERE l.country = ?1", nativeQuery = true)
+    List<Cinema> readAllCinemasByLocation(String locationCountry);
     //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema WHERE name ILIKE concat('%', ?1, '%') OR sponsored_name ILIKE concat ('%', ?1, '%')", nativeQuery = true)
+    List<Cinema> readCinemasByNameAndSponsored
+    (@Param("pattern") String pattern);
 
     //Write a native query to sort all cinemas by name
+    @Query(value = "SELECT * FROM cinema ORDER BY name", nativeQuery = true)
+    List<Cinema> orderByName();
 
     //Write a native query to distinct all cinemas by sponsored name
+    @Query(value = "SELECT DISTINCT sponsored_name FROM cinema", nativeQuery = true)
+    List<Cinema> orderByDistinctSponsoredName();
 
 }
--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
@@ -1,34 +1,54 @@
 package com.cydeo.repository;
 
+import com.cydeo.entity.Movie;
 import com.cydeo.entity.MovieCinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to read movie cinema with id
+    Optional<MovieCinema> findAllById(Long id);
 
     //Write a derived query to count all movie cinemas with a specific cinema id
-
+    Integer countAllByCinemaId(Long cinemaId);
     //Write a derived query to count all movie cinemas with a specific movie id
+    Integer countByMovieId(Long movieId);
 
     //Write a derived query to list all movie cinemas with higher than a specific date
+    List<MovieCinema> findByDateTimeAfter(LocalDateTime dateTime);
 
     //Write a derived query to find the top 3 expensive movies
+    List<MovieCinema> findFirst3ByOrderByMoviePriceDesc();
 
     //Write a derived query to list all movie cinemas that contain a specific movie name
+    List<MovieCinema> findAllByMovieNameContaining(String name);
 
     //Write a derived query to list all movie cinemas in a specific location
+    List<MovieCinema> findAllByCinemaLocationName(String name);
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to list all movie cinemas with higher than a specific date
+    @Query("SELECT mc FROM MovieCinema mc WHERE mc.dateTime > ?1")
+    List<MovieCinema> findALlByDateTimeAfter(LocalDateTime dateTime);
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to count all movie cinemas by cinema id
+    @Query(value = "SELECT COUNT(*) FROM movie_cinema WHERE cinema_id = ?1", nativeQuery = true)
+    Integer countMovieCinemasBy(Long cinemaId);
 
     //Write a native query that returns all movie cinemas by location name
-
+    @Query(value = "SELECT * FROM movie_cinema mc JOIN cinema c on mc.cinema_id = c.cinema_id" +
+            "JOIN location l on c.location_id = l.id SHERE l.name = ?1", nativeQuery = true)
+    List<MovieCinema> findALlByLocationName(@Param("name") String locationName);
 }
